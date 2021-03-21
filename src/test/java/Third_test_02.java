5b19634fe0dd6ac7e589733349294f71168c018a
Pulsaronline <beslite@gmail.com>
2021-03-21 15:34:32 +0300

    New method .dragAndDropTo implemented

--- src/test/java/Third_test_02.java
+++ src/test/java/Third_test_02.java
@@ -1,14 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Third_test_02 {
@@ -22,20 +17,22 @@ public class Third_test_02 {
     }
 
     @Test
-    void selenideSearchTest() {
+    void DragAndDropSelenideTest() {
         //- Откройте https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        //- Перенесите прямоугольник А на место В
 
-        Selenide.actions().clickAndHold($("#column-a")).moveToElement($("#column-b"),100,0).release().perform();
+        //- Перенесите прямоугольник А на место В
+        //- Метод "По старинке". Забагован, координаты #column-b перехватываются указателем мыши!
         //Selenide.actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
-        //Selenide.actions(clickAndHold($("#column-a")).moveByOffset(300,0).release().perform();
-        //$("#column-a").dragAndDropTo($("#column-b"));
+        sleep(1800);
+        //- Рабочий метод
+        $("#column-a").dragAndDropTo($("#column-b"));
+        sleep(1800);
+        //- Проверьте, что прямоугольники действительно поменялись
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
-        //- Проверьте, что прямоугольники действительно поменялись
-        //P.S. В Selenide есть команда $(element).dragAndDrop($(to-element)),
-        // проверьте работает ли тест, если использовать её вместо actions()
 
+        //- Закрываем https://the-internet.herokuapp.com/drag_and_drop
+        Selenide.closeWebDriver();
     }
 }
